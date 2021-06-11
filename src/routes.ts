4ef3d29dd6e3067651ba55dd0179b40c36d856f6
Wito Delnat <wito.delnat@gmail.com>
2021-06-11 18:37:37 +0200

    fix: simplify keat-server

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { definitions } from "./handlers/definitions";
-import { isEnabled } from "./handlers/isEnabled";
-import { user } from "./handlers/user";
 
 export async function routes(fastify: FastifyInstance) {
   fastify.register(definitions);
-  fastify.register(isEnabled);
-  fastify.register(user);
 }
